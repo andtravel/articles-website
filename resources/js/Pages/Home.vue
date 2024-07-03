6c1c9de1403b80312a120bf8db4700647ee31c8d
andrey <anovts@gmail.com>
2024-07-04 02:17:49 +0300

    Setup inertia.js, vue.js, bootstrap.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello World!');
+</script>
+
+<template>
+    <div class="container">
+        <div class="mb-3 mt-3 text-center">
+            <h1 class="bg-success text-warning">{{ message }}</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
